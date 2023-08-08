222ca1b242c4d6cc2d463af791c357be9c740c67
Irfanwani <irfanwani347@gmail.com>
2023-08-08 11:39:23 +0530

    class3b

--- class3b.c
+++ class3b.c
@@ -3,15 +3,30 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, j, res;
     printf("Please enter the number which you want the table of: ");
     scanf("%d", &i);
 
-    for (j = 1; j <= 10; j++) {
+    // using for loop
+    for (j = 1; j <= 10; j++)
+    {
         res = i * j;
 
         printf("%d x %d = %d\n", i, j, res);
     }
+
+    // using while loop
+    j = 1;
+    while (j <= 10)
+    {
+        res = i * j;
+
+        printf("%d x %d = %d\n", i, j, res);
+        j++;
+    };
+    
+
     return 0;
 }
