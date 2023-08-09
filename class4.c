dfc9dde3f1fbb17788b1412c24355ae9cc6e34e1
Irfanwani <irfanwani347@gmail.com>
2023-08-09 10:41:56 +0530

    generate the table of the given number and store the result in an array and then display the result

---
+++ class4.c
@@ -0,0 +1,18 @@
+// Q) generate the table of the given number and store the result in an array and then display the result
+#include <stdio.h>
+
+int main() {    
+    int res[10], num, i;
+    printf("Enter the number to get the table: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= 10; i++) {
+        res[i-1] = num*i;
+    };
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, res[i-1]);
+    }
+
+    return 0;
+}
